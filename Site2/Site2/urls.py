8e139951b020a4d4c74ce1d6147eb2c7b9aee8b5
SB1600Python <129617686+SB1600Python@users.noreply.github.com>
2023-04-29 17:57:54 +0300

    12

---
+++ Site2/Site2/urls.py
@@ -0,0 +1,39 @@
+"""Site2 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from blog.views import *
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index, name='home'),
+    path('<int:pk>/', detail, name='detail'),
+    path('login', view_login, name='login'),
+    # path('signup', view_signup, name= 'signup'),
+    path('register', RegisterView.as_view(), name='register'),
+    # path('logout', logout_view, name='logout'),
+    # path('creatPost', creat_post, name='creat'),
+    path('creat/', PostView.as_view(), name='creat'),
+
+    
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ Site2/blog/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.urls import reverse
+
+
+# Create your models here.
+class Post(models.Model):
+     title = models.CharField(max_length=255)
+     text = models.TextField()
+     image = models.ImageField(upload_to='photos/%Y/%m/%d', null=True, blank=True)
+     category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+     
+
+     def __str__(self):
+          return self.title
+     def get_absolute_url(self):
+          return reverse('home')
+
+class Category(models.Model):
+     name = models.CharField(max_length=255)
+
+     def __str__(self):
+          return self.name
